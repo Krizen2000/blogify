01ef17164227abae3435d99ab09367afbbe225e6
Krizen Knoz <krizenknoz@gmail.com>
2023-03-06 17:36:52 +0530

    fix: Added profession entry to User model

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
+  profession: { type: String, required: true },
   phoneNumber: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
