551db2b487a679ead757ad4509b478b3b23b057b
Krizen Knoz <krizenknoz@gmail.com>
2023-03-06 18:08:33 +0530

    chore: No use of username in params as no admin

--- api/src/routes/userRoute.js
+++ api/src/routes/userRoute.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const userController = require("../controllers/userController");
 const verifyToken = require("../middleware/verifyToken");
 
-router.get("/:username", verifyToken, userController.getUserInfoHandler);
-router.put("/:username", verifyToken, userController.updateUserInfoHandler);
-router.delete("/:username", verifyToken, userController.deleteUserInfoHandler);
+router.get("/", verifyToken, userController.getUserInfoHandler);
+router.put("/", verifyToken, userController.updateUserInfoHandler);
+router.delete("/", verifyToken, userController.deleteUserInfoHandler);
 
 module.exports = router;
