51537601831df8d199fed92cbec64ed983ef7fd8
Krizen Knoz <krizenknoz@gmail.com>
2023-05-13 12:59:56 +0530

    chore: Renaming attributes

--- api/src/models/Community.js
+++ api/src/models/Community.js
@@ -4,7 +4,7 @@ const CommunitySchema = new mongoose.Schema(
   {
     communityId: { type: String, required: true, unique: true },
     creator: { type: String, required: true },
-    imageUrl: { type: String, required: false },
+    image: { type: String, required: false },
     description: { type: String, required: true },
   },
   { timestamps: false }
