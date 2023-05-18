eec4ac5d39e89fa6d8e0a5ea0c0e1b68a32d1527
Krizen Knoz <krizenknoz@gmail.com>
2023-05-18 18:15:12 +0530

    chore: Change website export directory

--- website/next.config.js
+++ website/next.config.js
@@ -4,4 +4,6 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer({});
+module.exports = withBundleAnalyzer({
+  distDir: "build",
+});
